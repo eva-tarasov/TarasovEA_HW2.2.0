c86836c783e15af411e9c1a393521eee75529442
Evgeniy Tarasov <tarasov@ucontent.ru>
2019-09-24 10:15:52 +0300

    Initial Commit

---
+++ TarasovEA_HW2.2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TarasovEA_HW2.2.0
+//
+//  Created by Евгений Тарасов on 24/09/2019.
+//  Copyright © 2019 Евгений Тарасов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
